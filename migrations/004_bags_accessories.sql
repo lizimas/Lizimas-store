73187e3701088f31a639f9a499ff41944d3458ab
lizimas <sentzalizimas@gmail.com>
2026-08-02 04:36:27 +0300

    Record migration 004

---
+++ migrations/004_bags_accessories.sql
@@ -0,0 +1,6 @@
+-- 004: Bags & Accessories child under Apparel & Boutique
+-- Applied local: 2026-08-02
+-- Applied Render: NOT YET
+
+INSERT INTO categories (name, description, parent_id, display_order)
+VALUES ('Bags & Accessories', 'Handbags, belts, wallets and fashion accessories.', 2, 6);
